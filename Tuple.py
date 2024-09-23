87e0c13314a44eaecf7940ca3ce0e3061565b59f
Saanvi305 <Saanvimehtauk@gmail.com>
2024-09-23 19:59:25 +0100

    Add files via upload

---
+++ Tuple.py
@@ -0,0 +1,21 @@
+#WAP to create a tuple and learn packing and unpacking,slicing a tuple
+Mytuple=("Saanvi",692023,"Oakwood street","London",1349)
+print (Mytuple)
+name,moblie_number,street_name,city,pin=Mytuple
+print (name)
+print (moblie_number)
+print (street_name)
+print(city)
+print (pin)
+Tuple1="Name","23","Favourite color","London"
+print (Tuple1)
+#Mytuple[0]="ABCD"
+print (Mytuple[0]) 
+letters=("J","E","T","L","E","A","R","N")
+print (letters[3:6])
+print(letters[:])
+for x in Mytuple:
+    print(x)
+newtuple=("dog",[4,5,7],(1,2,3))
+newtuple=("dog",[7,9,2],(11,22,33))
+print (newtuple)
